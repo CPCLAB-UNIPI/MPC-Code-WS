85e76603995a7bfab2b648a365e1467c7a005daa
Marco Vaccari <>
2025-06-06 11:22:38 +0200

    Fixing Exercise Trace

--- MPC_code/Ex_PC25WS.py
+++ MPC_code/Ex_PC25WS.py
@@ -17,18 +17,18 @@ from Utilities import*
 ### 1) Simulation Fundamentals
 
 # 1.1) Simulation discretization parameters
-Nsim =  # Simulation length
+Nsim = 201 # Simulation length
 
-N =     # Horizon
+N =  10  # Horizon
 
-h =  # Time step
+h = 0.25 # Time step (min)
 
 # 3.1.2) Symbolic variables
-xp = SX.sym("xp", ) # process state vector       
-x = SX.sym("x", )  # model state vector          
-u = SX.sym("u", )  # control vector              
-y = SX.sym("y", )  # measured output vector      
-d = SX.sym("d", )  # disturbance                     
+xp = SX.sym("xp", 2) # process state vector       
+x = SX.sym("x", 2)  # model state vector          
+u = SX.sym("u", 1)  # control vector              
+y = SX.sym("y", 2)  # measured output vector      
+d = SX.sym("d", 2)  # disturbance                     
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 ### 2) Process and Model construction 
@@ -64,29 +64,38 @@ def User_fxp_Cont(x,t,u,pxp,pxmp):
     """ 
         
     ## System Dynamics 
-    
+    C, T = x[0], x[1]
+    Tc = u
+
+    k = km*np.exp(-EoR*(1/T-1/Tm)) # kinetic constant
+
+
+
+    dCAdt =  1/tau*(CA0 - C) - k*C #mass balance
+    dTdt = 1/tau*(T0-T) - deltaH/(rho*Cp)*k*C + 2*UA_V/(rho*Cp)*(Tc-T) #thermal balance
+  
     return vertcat(dCAdt, dTdt)
 
 Mx = 10 # Number of elements in each time step 
 
 # Output map
-def User_fyp(x,u,t,pyp,pymp):
-    """
-    SUMMARY:
-    It constructs the function User_fyp for the non-linear case
+# def User_fyp(x,u,t,pyp,pymp):
+#     """
+#     SUMMARY:
+#     It constructs the function User_fyp for the non-linear case
     
-    SYNTAX:
-    assignment = User_fyp(x,t)
+#     SYNTAX:
+#     assignment = User_fyp(x,t)
   
-    ARGUMENTS:
-    + x             - State variable
-    + t             - Variable that indicate the current iteration
+#     ARGUMENTS:
+#     + x             - State variable
+#     + t             - Variable that indicate the current iteration
     
-    OUTPUTS:
-    + fy_p      - Non-linear plant function     
-    """ 
+#     OUTPUTS:
+#     + fy_p      - Non-linear plant function     
+#     """ 
     
-    return fy_p
+#     return fy_p
 
 # White Noise
 # R_wn = 1e-4*np.array([[1.0, 0.0], [0.0, 1.0]]) # Output white noise covariance matrix
@@ -110,33 +119,46 @@ def User_fxm_Cont(x,u,d,t,px):
     OUTPUTS:
     + x_model       - Non-linear model function     
     """ 
-     
+
+    C, T = x[0], x[1]
+    Tc = u
+
+    k = km*np.exp(-EoR*(1/T-1/Tm)) # kinetic constant
+
+
+
+    dCAdt =  1/tau*(CA0 - C) - k*C #mass balance
+    dTdt = 1/tau*(T0-T) - deltaH/(rho*Cp)*k*C + 2*UA_V/(rho*Cp)*(Tc-T) #thermal balance
   
     return vertcat(dCAdt, dTdt)
 
 # Output Map
-def User_fym(x,u,d,t,py):
-    """
-    SUMMARY:
-    It constructs the function fy_m for the non-linear case
+# def User_fym(x,u,d,t,py):
+#     """
+#     SUMMARY:
+#     It constructs the function fy_m for the non-linear case
     
-    SYNTAX:
-    assignment = User_fym(x,u,d,t)
+#     SYNTAX:
+#     assignment = User_fym(x,u,d,t)
   
-    ARGUMENTS:
-    + x,d           - State and disturbance variable
-    + t             - Variable that indicate the current iteration
+#     ARGUMENTS:
+#     + x,d           - State and disturbance variable
+#     + t             - Variable that indicate the current iteration
     
-    OUTPUTS:
-    + fy_p      - Non-linear plant function     
-    """ 
+#     OUTPUTS:
+#     + fy_p      - Non-linear plant function     
+#     """ 
     
     
-    return fy_model
-    
+#     return fy_model
+StateFeedback = True   
+# Fp_nominal = True
+ssjacid = True # linearize the model at the starting point 
 
 # 2.3) Disturbance model for Offset-free control
-offree = "no" 
+offree = "lin" 
+Bd = np.zeros((2,2))
+Cd = np.eye(2)
 
 
 # 2.4) Initial condition
@@ -148,9 +170,15 @@ u0 = np.array([300])
 ### 3) State Estimation
  
 # what is the minimum needed for estimating disturbance in LIN?
-
+# lue = True # Luenberger observer 
 
 # what is the minimumneeded for estimating disturbance in NL?
+ekf = True
+Qx_kf = 1.0e-4*np.eye(x.size1())
+Qd_kf = np.eye(d.size1())
+Q_kf = scla.block_diag(Qx_kf, Qd_kf)
+R_kf = 1.0e-5*np.eye(y.size1())
+P0 = 1e-3*np.ones((x.size1()+d.size1(),x.size1()+d.size1())) 
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -172,29 +200,36 @@ def defSP(t):
     + ysp, usp, xsp - Input, output and state setpoint values      
     """ 
     
+    if t<25: 
+        ysp = np.array([0.2, 400])
+    else: 
+        ysp = np.array([0.15, 400])
+    usp = np.zeros(1)
+    xsp = np.zeros(2)
+
     return [ysp, usp, xsp]
     
 # 4.2) Bounds constraints
 ## Input bounds
-umin = 
-umax = 
+umin = 220
+umax = 400
 
 ## State bounds
-xmin = 
-xmax = 
+# xmin = 
+# xmax = 
 
 # ## Output bounds
-ymin = 
-ymax = 
+ymin = np.array([0, 220])
+# ymax = 
 
 
 
 # 4.3) Steady-state optimization : objective function
-Qss =  #Output matrix
-Rss = # Control matrix
+Qss = np.array([[10.0, 0.0], [0.0, 1.0e-6]])  #Output matrix
+Rss = 0 # Control matrix
 
 # 4.4) Dynamic optimization : objective function 
-Q = #
-S = #np.array([[0.1, 0.0], [0.0, 0.1]])
+Q = np.array([[10.0, 0.0], [0.0, 1.0e-6]])#
+S = 1e-5
 
 pathfigure = 'MPC_Images/'
